397d24a6d008131b7197b4b3ea63e47861acf8ad
shanghaik168@gmail.com <Shanghaik1998>
2023-10-04 18:02:50 +0700

    Add project files.

---
+++ BauCuaGame/Form1.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace BauCuaGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lb_Huou = new System.Windows.Forms.Label();
+            this.lb_Ga = new System.Windows.Forms.Label();
+            this.lb_Ca = new System.Windows.Forms.Label();
+            this.lb_Tom = new System.Windows.Forms.Label();
+            this.lb_Cua = new System.Windows.Forms.Label();
+            this.lb_Bau = new System.Windows.Forms.Label();
+            this.ptb_Huou = new System.Windows.Forms.PictureBox();
+            this.ptb_Ga = new System.Windows.Forms.PictureBox();
+            this.ptb_Ca = new System.Windows.Forms.PictureBox();
+            this.ptb_Tom = new System.Windows.Forms.PictureBox();
+            this.ptb_Cua = new System.Windows.Forms.PictureBox();
+            this.ptb_Bau = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ptb_R3 = new System.Windows.Forms.PictureBox();
+            this.ptb_R2 = new System.Windows.Forms.PictureBox();
+            this.ptb_R1 = new System.Windows.Forms.PictureBox();
+            this.rb_10K = new System.Windows.Forms.RadioButton();
+            this.rb_20K = new System.Windows.Forms.RadioButton();
+            this.rb_100K = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_Money = new System.Windows.Forms.Label();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Huou)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Ga)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Ca)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Tom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Cua)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Bau)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_R3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_R2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_R1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lb_Huou);
+            this.groupBox1.Controls.Add(this.lb_Ga);
+            this.groupBox1.Controls.Add(this.lb_Ca);
+            this.groupBox1.Controls.Add(this.lb_Tom);
+            this.groupBox1.Controls.Add(this.lb_Cua);
+            this.groupBox1.Controls.Add(this.lb_Bau);
+            this.groupBox1.Controls.Add(this.ptb_Huou);
+            this.groupBox1.Controls.Add(this.ptb_Ga);
+            this.groupBox1.Controls.Add(this.ptb_Ca);
+            this.groupBox1.Controls.Add(this.ptb_Tom);
+            this.groupBox1.Controls.Add(this.ptb_Cua);
+            this.groupBox1.Controls.Add(this.ptb_Bau);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(671, 405);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Khu vực đặt cược";
+            // 
+            // lb_Huou
+            // 
+            this.lb_Huou.AutoSize = true;
+            this.lb_Huou.Location = new System.Drawing.Point(540, 362);
+            this.lb_Huou.Name = "lb_Huou";
+            this.lb_Huou.Size = new System.Drawing.Size(26, 31);
+            this.lb_Huou.TabIndex = 11;
+            this.lb_Huou.Text = "0";
+            // 
+            // lb_Ga
+            // 
+            this.lb_Ga.AutoSize = true;
+            this.lb_Ga.Location = new System.Drawing.Point(300, 362);
+            this.lb_Ga.Name = "lb_Ga";
+            this.lb_Ga.Size = new System.Drawing.Size(26, 31);
+            this.lb_Ga.TabIndex = 10;
+            this.lb_Ga.Text = "0";
+            // 
+            // lb_Ca
+            // 
+            this.lb_Ca.AutoSize = true;
+            this.lb_Ca.Location = new System.Drawing.Point(48, 362);
+            this.lb_Ca.Name = "lb_Ca";
+            this.lb_Ca.Size = new System.Drawing.Size(26, 31);
+            this.lb_Ca.TabIndex = 9;
+            this.lb_Ca.Text = "0";
+            // 
+            // lb_Tom
+            // 
+            this.lb_Tom.AutoSize = true;
+            this.lb_Tom.Location = new System.Drawing.Point(540, 178);
+            this.lb_Tom.Name = "lb_Tom";
+            this.lb_Tom.Size = new System.Drawing.Size(26, 31);
+            this.lb_Tom.TabIndex = 8;
+            this.lb_Tom.Text = "0";
+            // 
+            // lb_Cua
+            // 
+            this.lb_Cua.AutoSize = true;
+            this.lb_Cua.Location = new System.Drawing.Point(300, 178);
+            this.lb_Cua.Name = "lb_Cua";
+            this.lb_Cua.Size = new System.Drawing.Size(26, 31);
+            this.lb_Cua.TabIndex = 7;
+            this.lb_Cua.Text = "0";
+            // 
+            // lb_Bau
+            // 
+            this.lb_Bau.AutoSize = true;
+            this.lb_Bau.Location = new System.Drawing.Point(48, 178);
+            this.lb_Bau.Name = "lb_Bau";
+            this.lb_Bau.Size = new System.Drawing.Size(26, 31);
+            this.lb_Bau.TabIndex = 6;
+            this.lb_Bau.Text = "0";
+            // 
+            // ptb_Huou
+            // 
+            this.ptb_Huou.Image = ((System.Drawing.Image)(resources.GetObject("ptb_Huou.Image")));
+            this.ptb_Huou.Location = new System.Drawing.Point(506, 219);
+            this.ptb_Huou.Name = "ptb_Huou";
+            this.ptb_Huou.Size = new System.Drawing.Size(140, 140);
+            this.ptb_Huou.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb_Huou.TabIndex = 5;
+            this.ptb_Huou.TabStop = false;
+            this.ptb_Huou.Click += new System.EventHandler(this.ptb_Huou_Click);
+            // 
+            // ptb_Ga
+            // 
+            this.ptb_Ga.Image = ((System.Drawing.Image)(resources.GetObject("ptb_Ga.Image")));
+            this.ptb_Ga.Location = new System.Drawing.Point(269, 219);
+            this.ptb_Ga.Name = "ptb_Ga";
+            this.ptb_Ga.Size = new System.Drawing.Size(140, 140);
+            this.ptb_Ga.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb_Ga.TabIndex = 4;
+            this.ptb_Ga.TabStop = false;
+            this.ptb_Ga.Click += new System.EventHandler(this.ptb_Ga_Click);
+            // 
+            // ptb_Ca
+            // 
+            this.ptb_Ca.Image = ((System.Drawing.Image)(resources.GetObject("ptb_Ca.Image")));
+            this.ptb_Ca.Location = new System.Drawing.Point(20, 219);
+            this.ptb_Ca.Name = "ptb_Ca";
+            this.ptb_Ca.Size = new System.Drawing.Size(140, 140);
+            this.ptb_Ca.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb_Ca.TabIndex = 3;
+            this.ptb_Ca.TabStop = false;
+            this.ptb_Ca.Click += new System.EventHandler(this.ptb_Ca_Click);
+            // 
+            // ptb_Tom
+            // 
+            this.ptb_Tom.Image = ((System.Drawing.Image)(resources.GetObject("ptb_Tom.Image")));
+            this.ptb_Tom.Location = new System.Drawing.Point(506, 35);
+            this.ptb_Tom.Name = "ptb_Tom";
+            this.ptb_Tom.Size = new System.Drawing.Size(140, 140);
+            this.ptb_Tom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb_Tom.TabIndex = 2;
+            this.ptb_Tom.TabStop = false;
+            this.ptb_Tom.Click += new System.EventHandler(this.ptb_Tom_Click);
+            // 
+            // ptb_Cua
+            // 
+            this.ptb_Cua.Image = ((System.Drawing.Image)(resources.GetObject("ptb_Cua.Image")));
+            this.ptb_Cua.Location = new System.Drawing.Point(269, 35);
+            this.ptb_Cua.Name = "ptb_Cua";
+            this.ptb_Cua.Size = new System.Drawing.Size(140, 140);
+            this.ptb_Cua.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb_Cua.TabIndex = 1;
+            this.ptb_Cua.TabStop = false;
+            this.ptb_Cua.Click += new System.EventHandler(this.ptb_Cua_Click);
+            // 
+            // ptb_Bau
+            // 
+            this.ptb_Bau.Image = ((System.Drawing.Image)(resources.GetObject("ptb_Bau.Image")));
+            this.ptb_Bau.Location = new System.Drawing.Point(20, 35);
+            this.ptb_Bau.Name = "ptb_Bau";
+            this.ptb_Bau.Size = new System.Drawing.Size(140, 140);
+            this.ptb_Bau.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb_Bau.TabIndex = 0;
+            this.ptb_Bau.TabStop = false;
+            this.ptb_Bau.Click += new System.EventHandler(this.ptb_Bau_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ptb_R3);
+            this.groupBox2.Controls.Add(this.ptb_R2);
+            this.groupBox2.Controls.Add(this.ptb_R1);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(15, 433);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(668, 239);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Khu vực quay số";
+            // 
+            // ptb_R3
+            // 
+            this.ptb_R3.Image = ((System.Drawing.Image)(resources.GetObject("ptb_R3.Image")));
+            this.ptb_R3.Location = new System.Drawing.Point(499, 59);
+            this.ptb_R3.Name = "ptb_R3";
+            this.ptb_R3.Size = new System.Drawing.Size(140, 140);
+            this.ptb_R3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb_R3.TabIndex = 8;
+            this.ptb_R3.TabStop = false;
+            // 
+            // ptb_R2
+            // 
+            this.ptb_R2.Image = ((System.Drawing.Image)(resources.GetObject("ptb_R2.Image")));
+            this.ptb_R2.Location = new System.Drawing.Point(262, 59);
+            this.ptb_R2.Name = "ptb_R2";
+            this.ptb_R2.Size = new System.Drawing.Size(140, 140);
+            this.ptb_R2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb_R2.TabIndex = 7;
+            this.ptb_R2.TabStop = false;
+            // 
+            // ptb_R1
+            // 
+            this.ptb_R1.Image = ((System.Drawing.Image)(resources.GetObject("ptb_R1.Image")));
+            this.ptb_R1.Location = new System.Drawing.Point(13, 59);
+            this.ptb_R1.Name = "ptb_R1";
+            this.ptb_R1.Size = new System.Drawing.Size(140, 140);
+            this.ptb_R1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb_R1.TabIndex = 6;
+            this.ptb_R1.TabStop = false;
+            // 
+            // rb_10K
+            // 
+            this.rb_10K.AutoSize = true;
+            this.rb_10K.Font = new System.Drawing.Font("Segoe UI", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.rb_10K.Location = new System.Drawing.Point(746, 65);
+            this.rb_10K.Name = "rb_10K";
+            this.rb_10K.Size = new System.Drawing.Size(152, 35);
+            this.rb_10K.TabIndex = 2;
+            this.rb_10K.TabStop = true;
+            this.rb_10K.Text = "10000 USD";
+            this.rb_10K.UseVisualStyleBackColor = true;
+            // 
+            // rb_20K
+            // 
+            this.rb_20K.AutoSize = true;
+            this.rb_20K.Font = new System.Drawing.Font("Segoe UI", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.rb_20K.Location = new System.Drawing.Point(746, 158);
+            this.rb_20K.Name = "rb_20K";
+            this.rb_20K.Size = new System.Drawing.Size(152, 35);
+            this.rb_20K.TabIndex = 3;
+            this.rb_20K.TabStop = true;
+            this.rb_20K.Text = "20000 USD";
+            this.rb_20K.UseVisualStyleBackColor = true;
+            // 
+            // rb_100K
+            // 
+            this.rb_100K.AutoSize = true;
+            this.rb_100K.Font = new System.Drawing.Font("Segoe UI", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.rb_100K.Location = new System.Drawing.Point(746, 254);
+            this.rb_100K.Name = "rb_100K";
+            this.rb_100K.Size = new System.Drawing.Size(165, 35);
+            this.rb_100K.TabIndex = 4;
+            this.rb_100K.TabStop = true;
+            this.rb_100K.Text = "100000 USD";
+            this.rb_100K.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(757, 378);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Số tiền của bạn";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lb_Money
+            // 
+            this.lb_Money.AutoSize = true;
+            this.lb_Money.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lb_Money.Location = new System.Drawing.Point(799, 433);
+            this.lb_Money.Name = "lb_Money";
+            this.lb_Money.Size = new System.Drawing.Size(41, 38);
+            this.lb_Money.TabIndex = 6;
+            this.lb_Money.Text = "...";
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Start.Location = new System.Drawing.Point(746, 545);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(221, 96);
+            this.btn_Start.TabIndex = 7;
+            this.btn_Start.Text = "Nơi đứng dậy";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            this.btn_Start.MouseLeave += new System.EventHandler(this.btn_Start_MouseLeave);
+            this.btn_Start.MouseHover += new System.EventHandler(this.btn_Start_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 685);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.lb_Money);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rb_100K);
+            this.Controls.Add(this.rb_20K);
+            this.Controls.Add(this.rb_10K);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Huou)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Ga)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Ca)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Tom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Cua)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Bau)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_R3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_R2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_R1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private PictureBox ptb_Huou;
+        private PictureBox ptb_Ga;
+        private PictureBox ptb_Ca;
+        private PictureBox ptb_Tom;
+        private PictureBox ptb_Cua;
+        private PictureBox ptb_Bau;
+        private GroupBox groupBox2;
+        private PictureBox ptb_R3;
+        private PictureBox ptb_R2;
+        private PictureBox ptb_R1;
+        private RadioButton rb_10K;
+        private RadioButton rb_20K;
+        private RadioButton rb_100K;
+        private Label label1;
+        private Label lb_Money;
+        private Button btn_Start;
+        private Label lb_Huou;
+        private Label lb_Ga;
+        private Label lb_Ca;
+        private Label lb_Tom;
+        private Label lb_Cua;
+        private Label lb_Bau;
+    }
+}
---
+++ BauCuaGame/Form1.cs
@@ -0,0 +1,100 @@
+﻿namespace BauCuaGame
+{
+    public partial class Form1 : Form
+    {
+        int limit = 0; //tổng số lượt chọn
+        public Form1()// constructor 
+        {
+            InitializeComponent();// Khởi tạo các component
+            ptb_Tom.Image = Image.FromFile(@"C:\Users\Acer\Desktop\BCTC\tom.png");
+            ptb_Tom.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Start_MouseHover(object sender, EventArgs e)
+        {
+            btn_Start.BackColor = Color.LightBlue;
+        }
+
+        private void btn_Start_MouseLeave(object sender, EventArgs e)
+        {
+            btn_Start.BackColor = Color.White;
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            Random r = new Random(); // tạo để dùng random
+            // Tạo 1 mảng chứa đường dẫn đến ảnh
+            string[] imagesURL = new string[]
+            {
+                @"C:\Users\Acer\Desktop\BCTC\Bau.jpg",
+                @"C:\Users\Acer\Desktop\BCTC\cua.jpg",
+                @"C:\Users\Acer\Desktop\BCTC\tom.png",
+                @"C:\Users\Acer\Desktop\BCTC\ca.png",
+                @"C:\Users\Acer\Desktop\BCTC\ga.jpg",
+                @"C:\Users\Acer\Desktop\BCTC\huou.jpg"
+            };
+            // Random ra 3 giá trị từ 0 đến 5 để lấy đường dẫn từ mảng URL theo vị trí
+            int r1 = r.Next(imagesURL.Length);
+            int r2 = r.Next(0, 5);
+            int r3 = r.Next(imagesURL.Length);
+            // Gán ảnh cho pictureBox
+            ptb_R1.Image = Image.FromFile(imagesURL[r1]);
+            ptb_R2.Image = Image.FromFile(imagesURL[r2]);
+            ptb_R3.Image = Image.FromFile(imagesURL[r3]);
+        }
+        public void DatCuoc(Label lb)
+        {
+            lb.Text = Convert.ToInt32(lb.Text) + 1 + "";
+            limit++; // Khi ấn vào thì tổng số lượt lựa chọn cũng tăng 1
+            if (limit > 3)
+            {
+                limit -= Convert.ToInt32(lb.Text); // giảm đúng bằng số lượng update về 0
+                lb.Text = "0";
+            }
+            // MessageBox.Show(limit.ToString()); // chỉ để check thôi
+        }
+
+        private void ptb_Bau_Click(object sender, EventArgs e)
+        {
+            DatCuoc(lb_Bau);
+            //lb_Bau.Text = Convert.ToInt32(lb_Bau.Text) + 1 + "";
+            //limit++; // Khi ấn vào thì tổng số lượt lựa chọn cũng tăng 1
+            //if (limit > 3)
+            //{
+            //    limit -= Convert.ToInt32(lb_Bau.Text); // giảm đúng bằng số lượng update về 0
+            //    lb_Bau.Text = "0";
+            //}
+            //MessageBox.Show(limit.ToString());
+        }
+
+        private void ptb_Cua_Click(object sender, EventArgs e)
+        {
+            DatCuoc(lb_Cua);
+        }
+
+        private void ptb_Tom_Click(object sender, EventArgs e)
+        {
+            DatCuoc(lb_Tom);
+        }
+
+        private void ptb_Ca_Click(object sender, EventArgs e)
+        {
+            DatCuoc(lb_Ca);
+        }
+
+        private void ptb_Ga_Click(object sender, EventArgs e)
+        {
+            DatCuoc(lb_Ga);
+        }
+
+        private void ptb_Huou_Click(object sender, EventArgs e)
+        {
+            DatCuoc(lb_Huou);
+        }
+    }
+}
